d5d7f9ee6e9cc287c8cbb70dd8a064efa0454f75
syed745 <syedb1997@gmail.com>
2022-09-12 17:22:46 +0500

    updated

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const mongoose = require('mongoose');
 const userRoute = require('./routes/routesTodo')
 
+
+const PORT = process.env.PORT || 5000;
+
 mongoose.connect(
 	'mongodb+srv://syedbilal:billy786@cluster0.jer9jii.mongodb.net/?retryWrites=true&w=majority',
 	(err) => {
@@ -18,4 +21,6 @@ mongoose.connect(
 app.use(express.json());
 app.use(cors());
 app.use('/', userRoute);
-app.listen(5000);
+app.listen(PORT, () => {
+	console.log(`Port Connected as ${PORT}`);
+});
